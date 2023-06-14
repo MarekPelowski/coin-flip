9261f15f4df3e9f359cd2dde84b165d70810b259
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-14 20:42:39 +0200

    made the coin flipping

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from random import randint, randrange
+from random import randint
+
+flip = 127
+status = ""
+
+def chance():
+    global status
+    x = randint(1, 2)
+    if x == 1:
+        status = "tails"
+    elif x == 2:
+        status = "heads"
+
+
+def calculation():
+    for x in range(flip):
+        chance()
+        print(status)
+
+
+calculation()
