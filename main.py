ef3a50b2f9f305e393e06692d5fe54facd9245ab
Marek Pelowski <marek.pelowski@protonmail.com>
2023-06-15 20:23:26 +0200

    added streak to the programm

--- main.py
+++ main.py
@@ -4,14 +4,28 @@ flip = int(input("Enter the amount of coin flips: "))
 status = ""
 tails = 0
 heads = 0
+streak_tails = 0
+streak_heads = 0
+streak_best_tails = []
+streak_best_heads = []
+
+
 
 
 def status_check():
-    global tails, heads
+    global tails, heads, streak_heads, streak_tails, streak_best_tails, streak_best_heads
     if status == "tails":
         tails = tails + 1
+        streak_tails = streak_tails + 1
+        streak_best_heads.insert(0, streak_heads)
+        streak_heads = 0
+
     elif status == "heads":
         heads = heads + 1
+        streak_heads = streak_tails + 1
+        streak_best_tails.insert(0, streak_heads)
+        streak_tails = 0
+
 
 
 def chance():
@@ -26,9 +40,12 @@ def chance():
 for x in range(flip):
     chance()
     status_check()
-    print(status, "\n")
+    print(status)
 tails_percent = (tails * 100) / flip
 heads_percent = (heads * 100) / flip
 
+
 print("tails: ", round(tails_percent, 4), "%")
 print("heads: ", round(heads_percent, 4), "%")
+print("\ntails streak: ", max(streak_best_tails))
+print("heads streak: ", max(streak_best_heads))
